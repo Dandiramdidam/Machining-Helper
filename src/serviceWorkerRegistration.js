3bca4c7ae8582bdf910a3b7d388c51f7775174ce
Dandiramdidam <dandiramdidam@gmail.com>
2024-02-25 15:45:09 +0100

    AlmostReady

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -14,7 +14,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
   
         if (isLocalhost) {
           checkValidServiceWorker(swUrl, config);
