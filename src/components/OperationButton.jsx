b3170a8fba8c55986ef46051010ad51a73996115
Dandiramdidam <dandiramdidam@gmail.com>
2024-03-11 11:54:41 +0100

    Small changes

--- src/components/OperationButton.jsx
+++ src/components/OperationButton.jsx
@@ -3,10 +3,12 @@ import { ACTIONS } from "./Calculator"
 export default function OperationButton({ dispatch, operation }) {
   return (
     <button
-      onClick={() =>
-        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
+      onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
     >
       {operation}
     </button>
   )
 }
+
+
+
